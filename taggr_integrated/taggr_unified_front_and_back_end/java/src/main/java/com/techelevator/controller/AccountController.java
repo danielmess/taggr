999d42f31f875ecb144cd4c1440ccae4f316549c
danielmess <d.roman.mess@gmail.com>
2021-11-16 16:17:14 -0500

    changed origin annotation to include https

--- taggr_integrated/taggr_unified_front_and_back_end/java/src/main/java/com/techelevator/controller/AccountController.java
+++ taggr_integrated/taggr_unified_front_and_back_end/java/src/main/java/com/techelevator/controller/AccountController.java
@@ -19,7 +19,7 @@ import java.util.Set;
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
-@CrossOrigin(origins = "http://tagger-drm.netlify.app", maxAge = 3600)
+@CrossOrigin(origins = "https://tagger-drm.netlify.app", maxAge = 3600)
 @PreAuthorize("isAuthenticated()")
 public class AccountController {
     private UserDAO userDAO;
